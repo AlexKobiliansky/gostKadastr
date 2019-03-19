40a866061094aea7f3476215854e62600c143263
alex <alex_kobilianskiy@bigmir.net>
2019-03-19 03:43:30 +0200

    header complete

--- app/js/common.js
+++ app/js/common.js
@@ -35,7 +35,6 @@ $(document).ready(function() {
         $(this).addClass('is-active')
     });
 
-
     API.bind( "close:start", function() {
         setTimeout(function() {
             mmenuBtn.removeClass( "is-active" );
@@ -44,6 +43,54 @@ $(document).ready(function() {
     /**
      * end mobile-mnu customization
      */
+
+    $('img.svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+
+
+    function heightses() {
+
+
+        if ($(window).width()>=480) {
+            $('.header-item-title').height('auto').equalHeights();
+            $('.header-item-desc').height('auto').equalHeights();
+        }
+    }
+
+    $(window).resize(function() {
+        heightses();
+    });
+
+    heightses();
 
 
     //E-mail Ajax Send
